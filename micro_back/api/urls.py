519d0c5eae0349a6cef1a8b5ed75810817ba96c9
Anita <ani_kim@kbtu.kz>
2024-04-23 22:30:39 +0500

    implemented cbv

--- micro_back/api/urls.py
+++ micro_back/api/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('comment', comment, name='comment'),
     path('posts', posts_list, name='posts_list'),
     path('posts/<int:pk>', post_detail, name='posts_detail'),
-    path('categories', categories_list, name='categories_list'),
-    path('categories/<int:pk>', category_detail, name='category_detail'),
-    path('users/<int:pk>', user_detail, name='user_detail'),
+    path('categories', CategoriesList.as_view(), name='categories_list'),
+    path('categories/<int:pk>', CategoryDetail.as_view(), name='category_detail'),
+    path('users/<int:pk>', UserDetail.as_view(), name='user_detail'),
     path('posts/<int:pk>/likes', likes_list, name='likes_list'),
     path('posts/<int:post_id>/users/<int:user_id>', check_like)
 ]
--- micro_back/api/views/category.py
+++ micro_back/api/views/category.py
@@ -1,22 +1,23 @@
 from ..serializers import CategorySerializer
 from ..models import Category
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-@api_view(['GET'])
-def categories_list(request):
-    if request.method == 'GET':
+
+class CategoriesList(APIView):
+    def get(self, request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(id=pk)
+        except Category.DoesNotExist as e:
+            return Response({"Error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def category_detail(request, pk):
-    try:
-        category = Category.objects.get(id=pk)
-    except Category.DoesNotExist as e:
-        return Response({"Error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    if request.method == "GET":
+    def get(self, request, pk):
+        category = self.get_object(pk)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
